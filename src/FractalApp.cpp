50a7bcb3655f09acfaf0604b406108230873eedd
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-01-13 18:08:41 +0000

    Fixed minor issues

--- src/FractalApp.cpp
+++ src/FractalApp.cpp
@@ -1,5 +1,6 @@
 #include "FractalFrame.h"
 #include "Buddhabrot.h"
+#include <iostream>
 
 class FractalApp: public wxApp{
 private:
@@ -7,13 +8,16 @@ private:
     bb *f;
 public:
     virtual bool OnInit(){
+        std::cerr << "+FractalApp::OnInit" << std::endl;
         wxInitAllImageHandlers();
         f = new bb(250000, 10000); //addPt, numIt
         fractalFrame = new FractalFrame(f);
         fractalFrame->Show(true);
+        std::cerr << "-FractalApp::OnInit" << std::endl;
         return true;
     }
     int OnExit(){
+        std::cerr << "+FractalApp::OnExit" << std::endl;
         delete f;
         return wxApp::OnExit();
     }
